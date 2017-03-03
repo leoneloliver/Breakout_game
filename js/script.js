6a594932cc6f6e9c8f0728a0c11911608e18b1fd
Leonel Oliveira <leoneloliver@gmail.com>
2017-03-03 09:48:28 -0500

    Including sounds
    modifying JS to include sounds.

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ var leftPressed = false;
 var brickRowCount = 8;
 var brickColumnCount = 5;
 var brickWidth = 97.5;
+
 var brickHeight = 20;
 var brickPadding = 0.2;
 var brickOffsetTop = 50;
@@ -22,6 +23,8 @@ var showStart = true;
 var paddleColor = "#054DFF";
 var lives = 3;
 var speed = 6;
+
+
 var bricks = [];
 for(c=0; c<brickColumnCount; c++) {
     bricks[c] = [];
@@ -29,8 +32,10 @@ for(c=0; c<brickColumnCount; c++) {
         bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+
 function keyDownHandler(e) {
     if(e.keyCode == 39) {
         rightPressed = true;
@@ -47,6 +52,7 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
+
 function collisionDetection() {
     for(c=0; c<brickColumnCount; c++) {
         for(r=0; r<brickRowCount; r++) {
@@ -56,13 +62,16 @@ function collisionDetection() {
                     dy = -dy;
                     b.status = 0;
                     score++;
+                    playSound('sound-block');
                     if(score==10){
                        paddleWidth = 150;
-                       paddleColor = 'green';                       
+                       paddleColor = 'green';
+                       
                     }else if(score == 30){
                        lives ++;
                        paddleWidth = 100;
-                       paddleColor = '#ccc';                       
+                       paddleColor = '#ccc';
+                       
                     }
                     if(score == brickRowCount*brickColumnCount) {
                         alert("YOU WIN, CONGRATS!");
@@ -73,11 +82,13 @@ function collisionDetection() {
         }
     }
 }
+
 function drawLives() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#fff";
     ctx.fillText("Lives: "+lives, canvas.width-65, 20);
 }
+
 function drawBall() {
     ctx.beginPath();
     //ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -102,7 +113,8 @@ function drawBricks() {
                 bricks[c][r].x = brickX;
                 bricks[c][r].y = brickY;
                 ctx.beginPath();
-                ctx.rect(brickX, brickY, brickWidth, brickHeight);             
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+              
                  if(c < 1){
                     ctx.fillStyle = "#FF5733";
                   }else if(c < 2){
@@ -114,6 +126,7 @@ function drawBricks() {
                   }else{
                     ctx.fillStyle = "#05A4FF";
                   }
+
                 ctx.fill();
                 ctx.closePath();
             }
@@ -125,6 +138,7 @@ function drawScore() {
     ctx.fillStyle = "#fff";
     ctx.fillText("Score: "+score, 8, 20);
 }
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBricks();
@@ -132,7 +146,8 @@ function draw() {
     drawPaddle();
     drawScore();
     drawLives();
-    collisionDetection();    
+    collisionDetection();
+    
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
@@ -142,6 +157,7 @@ function draw() {
     else if(y + dy > canvas.height-ballRadius) {
         if(x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
+            playSound('sound-paddle');
         }
         else {
             lives--;
@@ -154,18 +170,23 @@ function draw() {
               dx = 2;
               dy = -2;
               paddleX = (canvas.width-paddleWidth)/2;
-            }           
+            }
+            
         }
-    }    
+    }
+    
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
         paddleX += 7;
     }
     else if(leftPressed && paddleX > 0) {
         paddleX -= 7;
-    }   
+    }
+    
     x += dx;
-    y += dy; 
+    y += dy;
+  
 }
+
 function handleMouseClick(){
   showStart = false;
   setInterval(draw, speed);
@@ -179,4 +200,16 @@ function drawStart() {
     
 }
 drawStart();
+
+
+function playSound(sound){
+  const audio = document.querySelector("."+sound);
+  audio.currentTime = 0;
+  audio.play();
+}
+
+
+
+
+
 canvas.addEventListener('mousedown', handleMouseClick);
